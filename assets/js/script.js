31eb277de62bde0d57a6fadd39c20eb4e2eeac8a
Andrew Faugno <andrewfaugno825@gmail.com>
2022-04-22 17:06:07 -0400

    created home page and set up basics

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+var startButton = document.querySelector("#start-btn");
+
+
+function startQuiz() {
+
+
+}
+
+function nextQuestion() {
+
+}
+
+function selectAnswer() {
+
+}
+
+startButton.addEventListener("click", startQuiz);
